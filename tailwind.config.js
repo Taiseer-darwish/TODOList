6d189668b40650f5839d3b4a7436760b0d2a67a7
Taiseer-Darwish <taiseerdarwish@gmail.com>
2024-06-05 04:06:31 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        Cairo:['Cairo'],
+        Comfortaa:['Comfortaa'],
+        Tomorrow:['Tomorrow'],
+       },
+    },
+  },
+  plugins: [],
+}
+
