9f2b1fd1a9a6493c449c78a3a7b0c568da56da69
Taiseer-Darwish <taiseerdarwish@gmail.com>
2024-06-07 03:41:08 +0300

    new edite

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
         Comfortaa:['Comfortaa'],
         Tomorrow:['Tomorrow'],
        },
+       colors:{
+        main:['red-200'],
+        second:['red-300'],
+        b1:['stone-900'],
+        b2:['stone-600'],
+       }
     },
   },
   plugins: [],
