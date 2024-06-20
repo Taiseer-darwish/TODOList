d7c6324ed5e95782580c359e1daf81ea6b2d5008
Taiseer-Darwish <taiseerdarwish@gmail.com>
2024-06-20 23:24:03 +0300

    new edite

--- dist/main.js
+++ dist/main.js
@@ -5,16 +5,10 @@ count = document.getElementById("count");
 popup = document.getElementById("popup");
 YESButton = document.getElementById("YES");
 NOButton = document.getElementById("NO");
-
-let x ='create';
+let mode ='create';
+let tasksNum;
 let y;
-let z=0;
-let w;
-let tNotcomplet;
 
-
-
-//console.log(inputText.value);
 
   // first step put all data in array
      let TaskData;
@@ -30,17 +24,21 @@ let tNotcomplet;
    function create(){
     let textContent =inputText.value;
 
-    // createb object
-    let task = { textContent: textContent };
+    // create object
+    let task = {
+       textContent: textContent,
+       status : true,
+       edit: true,
+       };
 
     if(textContent != ''){
-      if(x === 'create'){
+      if(mode === 'create'){
         //push data to array
         TaskData.push(task);
       }else{
         //edit elelment
        TaskData[y]=task;
-       x ='create';
+       mode ='create';
       }
     }     
     
@@ -54,7 +52,6 @@ let tNotcomplet;
     showData()
     CountTextContent()
 
-    
 }
 
    //Run create function
@@ -64,39 +61,91 @@ let tNotcomplet;
    // showData
     function showData(){
     let content = '';
-    w = TaskData.length -1;
+    tasksNum = TaskData.length;
+    let incompleteTasks = 0;
 
-    for(let i=1; i<TaskData.length; i++){
+    for(let i=0; i<TaskData.length; i++){
 
-      let taskId = `task_${i}`;
+      let paragraph = `paragraph_${i}`;
       let editId = `edit_${i}`;
-    
+      let iconClass;
+      let editClass;
+
+      if (TaskData[i].status) {
+        iconClass = "fa-regular fa-circle";
+        editClass = "fa-solid fa-pencil";
+      } else {
+        iconClass = "fa-solid fa-circle-check";
+        editClass = "";
+        incompleteTasks++;
+      }  
+
         content +=`
          <div id="taskContent" class="taskContent">
                 <div class="X">
-                <div>${i}</div>  
-                  <i id="${taskId}" onclick="rightMatk('${taskId}', '${editId}')" class="fa-regular fa-circle"></i>
-                   <p id="textTask">${TaskData[i].textContent}</p>
+                <div>${i+1}</div>
+                   <div class="icon" onclick="toggleStatus(${i})"> 
+                   <i class="${iconClass}"></i>
+                   </div> 
+                   <p id="${paragraph}">${TaskData[i].textContent}</p>
                </div>
                 <div class="Y">
-                   <i onclick="edite(${i})" id="${editId}" class="fa-solid fa-pencil"></i>
-                   <i onclick="del(${i})" id="deletei" class="fa-solid fa-trash"></i> 
+                   <i onclick="edite('${i}','${paragraph}','${editId}')" id="${editId}" class="${editClass}"></i>
+                   <i onclick="del(${i})" id="deleteid" class="fa-solid fa-trash"></i> 
                 </div>
                </div>
              `
         }
+        tasksNum -= incompleteTasks;
 
       parenContent.innerHTML= content;
 }
     //Run showData 
      showData()
 
-      //Edite data
-      function edite(i) {
-        inputText.value=TaskData[i].textContent;
-        y=i;
-        x='edite';
+     function toggleStatus(i) {
+      TaskData[i].status = !TaskData[i].status;
+      localStorage.setItem('TASK', JSON.stringify(TaskData));
+
+      showData();
     }
+
+      //function Edit data
+      function edite(i,paragraph,editId) {
+         
+     //call paragraph 
+     let p = document.getElementById(paragraph);
+     let text = TaskData[i].textContent;
+
+
+     if(TaskData[i].edit){
+
+     //create input
+     let input = document.createElement("input");
+     input.setAttribute("type", "text");
+     input.setAttribute("value", text);
+
+     //replase paragraph by iput
+     p.parentNode.replaceChild(input, p);
+
+     //call edit icon and create new icon
+     let editIcon = document.getElementById(editId);
+     let soliedIcon = document.createElement('i');
+     soliedIcon.className = 'fa-solid fa-circle-check';
+
+     //replace editIcon icon 
+     editIcon.parentNode.replaceChild(soliedIcon, editIcon);
+
+      soliedIcon.addEventListener("click", function (i) {
+        //toggle editobject status To change what is needed
+        TaskData[i].edit= !TaskData[i].edit;
+        input.parentNode.replaceChild(p,input);
+        p.innerHTML = input.value;
+        soliedIcon.parentNode.replaceChild(editIcon, soliedIcon);  
+        localStorage.setItem("TASK", JSON.stringify(TaskData));
+      })
+    }}
+
 
      //function Delete data 
       function deleteData(i) {
@@ -125,65 +174,30 @@ let tNotcomplet;
       popup.style.display='none';
      });
 
+     function updateTasksCount() {
+  let incompleteTasks = 0;
 
-   // ** function rightMatk **
-    function rightMatk(taskId, editId) {
-
-  let markIcon = document.getElementById(taskId);
-  let soliedIcon = document.createElement('i');
-  soliedIcon.className = 'fa-solid fa-circle-check';
-
-  //replace markIcon icon by solid icon
-  markIcon.parentNode.replaceChild(soliedIcon, markIcon);
-  z+=1;
-  tNotcomplet= w - z;
-  TextCount.textContent = `You Have Task (${tNotcomplet}) To Complete`;
-
-  // remove edit icon when complete task
-  removeEdite();
-
-  function removeEdite() {
-    let rEdite = document.getElementById(editId);
-    rEdite.style.display = 'none';
+  for (let i = 0; i < TaskData.length; i++) {
+    if (!TaskData[i].status) {
+      incompleteTasks++;
+    }
   }
 
-  function jj() {
-    //replace solid icon by markIcon icon
-    soliedIcon.parentNode.replaceChild(markIcon, soliedIcon);
-
-    //show edit icon 
-    let rEdite = document.getElementById(editId);
-    rEdite.style.display = 'block';
-    
-  }
-  //run function when click on solied icon
-  soliedIcon.addEventListener("click", function () {
-    jj();
-    z=z-1;
-    tNotcomplet= w - z;
-    TextCount.textContent = `You Have Task (${tNotcomplet}) To Complete`;
-  });
- 
+  tasksNum = incompleteTasks;
+  CountTextContent();
 }
-// end function rightmark--------------------------------
-
 
    //create TextCount
    let TextCount = document.createElement("P");
    TextCount.className = 'TextCount';
    //function textCount
    function CountTextContent(){
-    TextCount.textContent=(`You Have Task (${w}) To Complete`);
+    TextCount.textContent=(`You Have (${tasksNum}) Task To Complete`);
 }   
    //run 
    CountTextContent();
    //inheart
    count.appendChild(TextCount);
-
-
-
-
-
 
 
 
